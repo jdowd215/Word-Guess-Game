b64d3801362e87359aad4aa2025baf49a7db8df8
JDowd <jamesddowd@outlook.com>
2018-08-24 22:07:17 -0700

    commit functions + events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,7 @@ var wordBank = [        // Word list
     "austin",
     "boston",
 ];
+console.log(wordBank);
 //player wins
 var wins = 0;
 //player losses
@@ -60,22 +61,57 @@ function resetGame(){
     for (var i = 0; i < wordBank[currentWord].length; i++) {
         chosenWord.push("_");
     }
-    
+
     // add underscores to the underscores array according to the length of the word
     // TODO:
     // we need to write syntax using the currentWord as our input and this should output an 
     // array of underscores according to the length of the currentWord
     // output --> ['_', '_', '_', '_', '_', '_', '_']
     // convert the underscore array into a string
-    // select the html and display our underscores
+    // select the html and display our underscores 
     // display guessesRemaining, wins, and losses
-    // }
+    document.getElementById("totalWins").innerHTML = wins;
+    document.getElementById("currentWord").innerHTML = "";
+        for (var i = 0; i < chosenWord.length; i++) {
+            document.getElementById("currentWord").innerHTML += chosenWord[i];
+        }
 
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    document.getElementById("guessedLetters").innerHTML = guessedLetters;
+        if (remainingGuesses <= 0) {
+        hasFinished = true;
+        }
+    
 
 // ===== EVENT LISTENERS ===== // 
 // on page load execute our reset function
 // reset()
 // use document.onKeyUp = function (event){
+    document.onkeyup = function(event) {
+        if(hasFinished) {
+            resetGame();
+            hasFinished = false;
+        }
+    }
+    
+    //function takes letter and finds all instances of appearance
+    //in the string and replces them in the guess word
+    function evaluateGuess(letter) {
+    //store positions of letter in string
+        var positions = [];
+
+    //loop thru word finding all instances of guessed letter, store in array    
+        for (var i = 0; i < wordBank[currentWord].length; i++) {
+            if (wordBank[currentWord].length; i++) {
+                positions.push(i);
+            }
+    //loop thru word and replace "_" with a letter       
+        else {
+            for (var i = 0; i < positions.length; i++) {
+                chosenWord[positions[i]] = letter;
+            }
+        }
+    }
     // store userGuess in a variable
     // userInput = <value from the event object>
 
